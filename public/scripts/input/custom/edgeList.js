cfd2b9e37c3db5530facfca8d2e2311d629378d2
Aman Doshi <amandoshi001@gmail.com>
2021-11-25 20:32:46 +0400

    Added format check, JS docstrings, post data

--- public/scripts/input/custom/edgeList.js
+++ public/scripts/input/custom/edgeList.js
@@ -1,5 +1,14 @@
 let lineCount = 1;
 
+/**
+ * @const - maximum number of possible edges
+ */
+const maxEdges = Math.floor(maxNumberOfNodes / 2);
+
+/**
+ * Update line count of text area
+ * @param {Object} object - reference to HTML text area
+ */
 function updateLineCount(object) {
 	// get current line count
 	const currentLineCount = countLines(object.value);
@@ -11,6 +20,11 @@ function updateLineCount(object) {
 	}
 }
 
+/**
+ * Count number of lines in a text
+ * @param {String} text
+ * @returns {Number} - number of lines in text
+ */
 function countLines(text) {
 	if (text == "") {
 		return 1;
@@ -19,6 +33,9 @@ function countLines(text) {
 	}
 }
 
+/**
+ * Display line count in HTML document
+ */
 function fillLineCount() {
 	// get read-only textarea
 	const lineObject = document.getElementById("lineNumber");
@@ -33,6 +50,9 @@ function fillLineCount() {
 	lineObject.value = lineString;
 }
 
+/**
+ * Synchronise the scrolls of input textarea and line count textarea
+ */
 function syncScroll() {
 	// textarea objects
 	const lineObject = document.getElementById("lineNumber");
@@ -42,13 +62,120 @@ function syncScroll() {
 	lineObject.scrollTop = inputObject.scrollTop;
 }
 
+/**
+ * Format check structure of edge list
+ * Convert edge list to adjacency matrix
+ * Post adjacency matrix for visualisation
+ */
 function submit() {
-	Swal.fire({
-		confirmButtonColor: "#0088a9",
-		icon: "error",
-		text: "Something went wrong...",
-		title: "An error occured",
-	});
+	const edges = document.getElementById("edgeList").value.split("\n");
+	const { validFormat, errorTitle, errorText, edgeList } = formatCheck(edges);
+
+	if (!validFormat) {
+		return alertError({
+			text: errorText,
+			title: errorTitle,
+		});
+	}
+
+	// compile data
+	let graph = new Graph(weighted);
+	graph.directed = directed;
+	graph.edgeList = edgeList;
+
+	const data = {
+		weighted,
+		matrix: JSON.stringify(graph.matrix),
+		nodeNames: JSON.stringify(graph.nodeNames),
+	};
+
+	postToUrl(data, "/visualise");
 }
 
-window.addEventListener("load", fillLineCount);
+/**
+ * Format check edge list and identify errors
+ * Reconstruct edge list
+ * @param {String[]} edges - all edge connections of graph
+ * @returns {Object} - edge list, validity of edge list, error message and title
+ */
+function formatCheck(edges) {
+	// determine valid length of edge connection
+	const validLength = weighted ? 3 : 2;
+
+	// check for too many edges
+	if (edges.length > maxEdges) {
+		return {
+			validFormat: false,
+			errorTitle: "Too Many Edges",
+			errorText: `Max number of edges: ${maxEdges}`,
+			edgeList: [],
+		};
+	}
+
+	let edgeList = Array();
+	for (let i = 0; i < edges.length; i++) {
+		let edgeData = edges[i].split(" ").filter((element) => element != "");
+
+		// check for valid edge formatting
+		if (
+			edgeData.length != validLength ||
+			edgeData[0] == edgeData[1] ||
+			!isWord(edgeData[0]) ||
+			!isWord(edgeData[1]) ||
+			(weighted && (!isInteger(edgeData[2]) || edgeData[2] == 0))
+		) {
+			return {
+				validFormat: false,
+				errorTitle: "Invalid Format",
+				errorText: `Edge ${i + 1} is incorrectly formatted`,
+				edgeList: [],
+			};
+		} else if (
+			edgeData[0].length > maxNodeNameLength ||
+			edgeData[1].length > maxNodeNameLength
+		) {
+			let message = `Edge ${i + 1} is incorrectly formatted. `;
+			message += `Node names cannot exceed ${maxNodeNameLength} characters.`;
+
+			return {
+				validFormat: false,
+				errorTitle: "Invalid Format!",
+				errorText: message,
+				edgeList: [],
+			};
+		} else if (weighted && parseInt(edgeData[2]) > maxEdgeWeight) {
+			let message = `Edge ${i + 1} is incorrectly formatted. `;
+			message += `Maximum edge weight is ${maxEdgeWeight}.`;
+
+			return {
+				validFormat: false,
+				errorTitle: "Invalid Format!",
+				errorText: message,
+				edgeList: [],
+			};
+		} else {
+			// construct edge list
+			edgeList.push(edgeData);
+		}
+	}
+
+	return {
+		validFormat: true,
+		errorTitle: "",
+		errorText: "",
+		edgeList: edgeList,
+	};
+}
+
+/**
+ * Setup HTML page
+ */
+window.addEventListener("load", () => {
+	const inputObject = document.getElementById("edgeList");
+
+	if (inputObject.value.split("\n").length == 1) {
+		fillLineCount();
+	} else {
+		updateLineCount(inputObject);
+	}
+});
