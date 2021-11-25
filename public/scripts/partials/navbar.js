fe78c444830c6250ee040ae268e90c641c759d15
Aman Doshi <amandoshi001@gmail.com>
2021-11-25 20:36:52 +0400

    Added JS docstrings

--- public/scripts/partials/navbar.js
+++ public/scripts/partials/navbar.js
@@ -1,3 +1,7 @@
+/**
+ * Add styling as CSS to page
+ * @param {String} styles - styles to add, structured as css
+ */
 function addStyle(styles) {
 	// Create style document
 	let css = document.createElement("style");
@@ -12,11 +16,17 @@ function addStyle(styles) {
 	document.getElementsByTagName("head")[0].appendChild(css);
 }
 
+/**
+ * Update the pixel width of navbar to fit entire window
+ */
 function updateNavbarWidth() {
 	const width = window.innerWidth;
 	addStyle(`.navbar { width: ${width}px }`);
 }
 
+/**
+ * Update navbar width dynamically when window width changes
+ */
 function setupNavbar() {
 	updateNavbarWidth();
 	window.addEventListener("resize", updateNavbarWidth);
