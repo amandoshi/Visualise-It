7f129d9f08012437b461b3e7442e09284edad33f
Aman Doshi <amandoshi001@gmail.com>
2021-11-25 20:30:09 +0400

    Added Cytoscape dependencies and new routes

--- app.js
+++ app.js
@@ -37,9 +37,29 @@ app.use(
 	express.static(path.join(__dirname, "/node_modules/sweetalert2"))
 );
 
+// cytoscape modules - allow public access
+app.use(
+	"/cytoscape",
+	express.static(path.join(__dirname, "/node_modules/cytoscape"))
+);
+app.use(
+	"/cytoscape-fcose",
+	express.static(path.join(__dirname, "/node_modules/cytoscape-fcose"))
+);
+app.use(
+	"/layout-base",
+	express.static(path.join(__dirname, "node_modules/layout-base"))
+);
+app.use(
+	"/cose-base",
+	express.static(path.join(__dirname, "node_modules/cose-base"))
+);
+
 // define routes
 app.use("/", require("./routes/homepage"));
+app.use("/information", require("./routes/information"));
 app.use("/input", require("./routes/input"));
+app.use("/visualise", require("./routes/visualise"));
 
 // 404 route
 app.use("*", require("./routes/404"));
